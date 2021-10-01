606fd66b9f1723456409eea6896c5d13916300b3
setsukyodayo <200098ve@yse-c.net>
2021-10-01 14:18:46 +0900

    セッション結果判定

--- nyuka.php
+++ nyuka.php
@@ -14,18 +14,17 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
 */
-
 if (session_status()==PHP_SESSION_NONE) {
 	session_start();
 }
 
-
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-// if (/* ③の処理を書く */){
-// 	//④SESSIONの「error2」に「ログインしてください」と設定する。
-// 	//⑤ログイン画面へ遷移する。
+ if ($_POST['mode']!="login"){
+ 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+	$error2="ログインしてください。";
+ 	//⑤ログイン画面へ遷移する。
 	header("Location:login.php");
-// }
+ }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
 //⑦データベースで使用する文字コードを「UTF8」にする
