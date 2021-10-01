e34cc2dff2bca085cdda7af7d3eaae2093b100d4
setsukyodayo <200098ve@yse-c.net>
2021-10-01 11:36:10 +0900

    機能追加

--- nyuka.php
+++ nyuka.php
@@ -13,27 +13,42 @@
 /*
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
- */
-if (/* ①.の処理を行う */) {
-	//②セッションを開始する
+*/
+
+if (session_status()==PHP_SESSION_NONE) {
+	session_start();
 }
 
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
-}
+// if (/* ③の処理を書く */){
+// 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+// 	//⑤ログイン画面へ遷移する。
+	header("Location:login.php");
+// }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
-
 //⑦データベースで使用する文字コードを「UTF8」にする
+$db_name ='zaiko2021_yse';
+$db_host='localhost';
+$db_port='3306';
+$db_user='zaiko2021_yse';
+$db_password = '2021zaiko';
+$dsn="mysql:dbname=($db_name }; host=($db_host}; charset=utf8; port=($db_port}";
+try{
+	$pdo=new PDO($dsn, $db_user, $db_password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO:: ERRMODE_EXCEPTION) ;
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false) ;
+}catch (PDOException $e){
+	echo"接綕失敗:".$e-›getMessage();
+	exit;
+}
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
-}
+// if(/* ⑧の処理を行う */){
+// 	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
+// 	//⑩在庫一覧画面へ遷移する。
+// }
 
 function getId($id,$con){
 	/* 
@@ -77,9 +92,9 @@ function getId($id,$con){
 			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 			 * 設定されていた場合はif文の中に入る。
 			 */ 
-			if(/* ⑬の処理を書く */){
-				//⑭SESSIONの「error」の中身を表示する。
-			}
+			// if(/* ⑬の処理を書く */){
+			// 	//⑭SESSIONの「error」の中身を表示する。
+			// }
 			?>
 			</div>
 			<div id="center">
@@ -99,7 +114,7 @@ function getId($id,$con){
 					/*
 					 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 					 */
-    				foreach(/* ⑮の処理を書く */){
+    				 foreach(/* ⑮の処理を書く */){
     					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
 					?>
 					<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
