e4d365f0ec8eb5b839bf0d15063d09c00a4fbdbc
arufa142 <200084zs@yse-c.net>
2021-10-15 10:32:08 +0900

    少しだけできたかも施楠豪

--- syukka.php
+++ syukka.php
@@ -14,9 +14,10 @@
  * 一致した場合はif文の中に入る。
  */
 // if (/* ①の処理を行う */) {
+if (session_status()==PHP_SESSION_NONE) {
 // 	//②セッションを開始する
 session_start();
-// }
+}
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
 if (empty($_SESSION['login'])){/* ③の処理を書く */
